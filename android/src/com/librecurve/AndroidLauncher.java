092dcea929ee345ffc0bb0504a1c9fcd0d3bb8b9
Hans Ole Hatzel <hhatzel@gmail.com>
2016-11-03 20:55:01 +0100

    Initial setup.

---
+++ android/src/com/librecurve/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.librecurve;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.librecurve.Librecurve;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Librecurve(), config);
+	}
+}
